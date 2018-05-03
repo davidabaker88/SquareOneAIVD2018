9f905871d32c81f29ffb8513e343ac770dbf555a
Taylor Dettling <tdettling@students.inghamisd.org>
2018-05-03 10:29:40 -0400

    Connected wires.

--- CustomLogic.v
+++ CustomLogic.v
@@ -1,4 +1,4 @@
-module CustomLogic (
+/module CustomLogic (
 				//INPUTS from board
 				LEDR,SW,GPIO_0,GPIO_1,ADC_CON,KEY,CLOCK_50,
 				//Inputs from processor
@@ -21,14 +21,15 @@ input wire [6:0]  driveSpeedPercentage
 input wire [7:0]  swivelDistance
 input wire        swivelDirection
 input wire        reverse
+input wire        EncoderReset
 
 output wire [31:0] encoderInCM				 
-output wire [8:0]  FRSensorInCM
-output wire [8:0]  FLSensorInC
+output wire [8:0]  RSensorInCM
+output wire [8:0]  LSensorInC
 output wire	[8:0]  RSensorInCM
 output wire [8:0]  LSensorInCM
 output wire [8:0]  BLSensorInCM
-output wire [8:0]  BRSensorInCM
+output wire [8:0]  RSensorInCM
 output wire [7:0]  leftMagnetic
 output wire [7:0]  rightMagnetic
 
@@ -37,30 +38,68 @@ output wire yellowLight
 output wire	greenLight
 output wire	GPSLat 			
 output wire GPSLong
+
+//wire encoderA;
+
+//assign encoderA = GPIO_0[0];
  
 
 Steering Steering(
+.CLOCK_50(CLOCK_50),
+.targetDirection(),
+.reset_Pos(),
+
+//outputs
+.targetDirection_Aid(),
+.reset_Pos_Aid()
 
 );
 
 Drive Drive(
-.A(),
-.B(),
-.CLOCK_50(),
-.Reset(),
-.EncoderReset(),
-.SWDirection(),
-.SWSpeed(),
+.A(GPIO_0[0]),
+.B(GPIO_0[1]),
+.CLOCK_50(CLOCK_50),
+.Reset(KEY[0]),
+.EncoderReset(EncoderReset),
+.SWDirection(reverse),
+.SWSpeed(driveSpeedPercentage),
 //output
-.EncoderInCM(),
-.MotorDirection(),
-.MotorPWM(),
-.MotorSpeed(),
+.EncoderInCM(GPIO_0[2]),
+.MotorDirection(GPIO_0[15]),
+.MotorPWM(GPIO_0[16]),
+.MotorSpeed()
 	
 );
 
 
 SonicSensorNetwork SonicSensorNetwork(
+//x6 echo
+.echo(GPIO_0[3]),
+.echo1(GPIO_0[4]),
+.echo2(GPIO_0[5]),
+.echo3(GPIO_0[6]),
+.echo4(GPIO_0[7]),
+.echo5(GPIO_0[8]),
+.CLOCK_50(CLOCK_50),
+.reset(KEY[0]),
+//outputs
+//x6 Trigger
+.Trigger(GPIO_0[9]),
+.Trigger1(GPIO_0[10]),
+.Trigger2(GPIO_0[11]),
+.Trigger3(GPIO_0[12]),
+.Trigger4(GPIO_0[13]),
+.Trigger5(GPIO_0[14]),
+
+
+
+//x6 DistanceInCM
+.DistanceInCM(RSensorInCM[0]),
+.DistanceInCM1(RSensorInCM[1]),
+.DistanceInCM2(RSensorInCM[2]),
+.DistanceInCM3(RSensorInCM[3]),
+.DistanceInCM4(RSensorInCM[4]),
+.DistanceInCM5(RSensorInCM[5])
 );
 
 Debug Debug(
