63f5a7871863acf45d40d0e073f9683c46535196
Taylor Dettling <tdettling@students.inghamisd.org>
2018-05-10 09:07:44 -0400

    Finished Custom Logic file

--- CustomLogic.v
+++ CustomLogic.v
@@ -1,57 +1,58 @@
-/module CustomLogic (
+module CustomLogic (
 				//INPUTS from board
 				LEDR,SW,GPIO_0,GPIO_1,ADC_CON,KEY,CLOCK_50,
 				//Inputs from processor
-				targetDirection,driveSpeedPercentage,swivelDistance,swivelDirection,reverse
+				targetDirection,driveSpeedPercentage,swivelDistance,swivelDirection,reverse,steeringReset
 			    //OUTPUTS to board
 				HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,
 				//Outputs to Processor
-				 encoderInCM,FRSensorInCM,FLSensorInC,RSensorInCM,LSensorInCM,RSensorInCM,BLSensorInCM,GPSLat,GPSLong,leftMagnetic,rightMagnetic,challengeSelect,
+				 encoderInCM,FRSensorInCM,FLSensorInC,RSensorInCM,LSensorInCM,RSensorInCM,BLSensorInCM,GPSLat,GPSLongleftMagnetic,rightMagnetic,challengeSelect,
 				 redLight,yellowLight,greenLight
 				) 
 				
-input wire [9:0]  LEDR
-input wire [9:0]  SW
-inout wire [39:0] GPIO_0
-inout wire [39:0] GPIO_1			
-input wire [3:0]  KEY
-input wire        CLOCK_50
-input wire [8:0]  targetDirection
-input wire [6:0]  driveSpeedPercentage
-input wire [7:0]  swivelDistance
-input wire        swivelDirection
-input wire        reverse
-input wire        EncoderReset
+input wire [9:0]  	LEDR;
+input wire [9:0]  	SW;
+inout wire [39:0] 	GPIO_0;
+inout wire [39:0] 	GPIO_1;		
+input wire [3:0]  	KEY;
+input wire        	CLOCK_50;
+input wire [8:0]  	targetDirection;
+input wire [6:0]  	driveSpeedPercentage;
+input wire [7:0]  	swivelDistance;
+input wire        	swivelDirection;
+input wire       	 	reverse;
+input wire        	EncoderReset;
+input wire 				steeringReset;
 
-output wire [31:0] encoderInCM				 
-output wire [8:0]  RSensorInCM
-output wire [8:0]  LSensorInC
-output wire	[8:0]  RSensorInCM
-output wire [8:0]  LSensorInCM
-output wire [8:0]  BLSensorInCM
-output wire [8:0]  RSensorInCM
-output wire [7:0]  leftMagnetic
-output wire [7:0]  rightMagnetic
+ 
+output wire [31:0] 	encoderInCM;				 
+output wire [8:0]  	FRSensorInCM
+output wire [8:0]  	FLSensorInCM;
+output wire	[8:0] 	RSensorInCM;
+output wire [8:0]  	LSensorInCM;
+output wire [8:0]  	BLSensorInCM;
+output wire [8:0]  	BRSensorInCM;
+output wire [7:0]  	leftMagnetic;
+output wire [7:0]  	rightMagnetic;
 
-output wire redLight
-output wire yellowLight
-output wire	greenLight
-output wire	GPSLat 			
-output wire GPSLong
+output wire 			redLight;
+output wire 			yellowLight;
+output wire				greenLight;
+output wire				GPSLat 	;		
+output wire 			GPSLong;
 
-//wire encoderA;
+wire [6:0] MotorSpeed;
 
-//assign encoderA = GPIO_0[0];
  
 
 Steering Steering(
 .CLOCK_50(CLOCK_50),
-.targetDirection(),
-.reset_Pos(),
+.targetDirection(targetDirection),
+.reset_Pos(steeringReset),
 
 //outputs
-.targetDirection_Aid(),
-.reset_Pos_Aid()
+.targetDirection_Ard(GPIO_0[17]),
+.reset_Pos_Ard(GPIO_0[18])
 
 );
 
@@ -67,7 +68,7 @@ Drive Drive(
 .EncoderInCM(GPIO_0[2]),
 .MotorDirection(GPIO_0[15]),
 .MotorPWM(GPIO_0[16]),
-.MotorSpeed()
+.MotorSpeed(MotorSpeed)
 	
 );
 
@@ -94,15 +95,38 @@ SonicSensorNetwork SonicSensorNetwork(
 
 
 //x6 DistanceInCM
-.DistanceInCM(RSensorInCM[0]),
-.DistanceInCM1(RSensorInCM[1]),
-.DistanceInCM2(RSensorInCM[2]),
-.DistanceInCM3(RSensorInCM[3]),
-.DistanceInCM4(RSensorInCM[4]),
-.DistanceInCM5(RSensorInCM[5])
+.DistanceInCM(FLSensorInCM), 
+.DistanceInCM1(FRSensorInCM), 
+.DistanceInCM2(RSensorInCM), 
+.DistanceInCM3(BRSensorInCM), 
+.DistanceInCM4(BLSensorInCM), 
+.DistanceInCM5(LSensorInCM) 
 );
 
 Debug Debug(
-);
- 
 
+.Clock_50(Clock_50),
+.Reset(KEY[0]),
+.driveSpeedPercentage(MotorSpeed),
+.encoderInCM(encoderInCM),
+.FRSensorInCM(FRSensorInCM),
+.FLSensorInCM(FLSensorInCM),
+.RSensorInCM(RSensorInCM),
+.LSensorInCM(LSensorInCM),
+.BLSensorInCM(BLSensorInCM),
+.GPSLat(GPSLat),				
+.GPSLong(GPSLong),
+.leftMagnetic(leftMagnetic),
+.rightMagnetic(rightMagnetic),
+.challengeSelect(challengeSelect),
+.redLight(redLight),
+.yellowLight(yellowLight),
+.greenLight(greenLight),
+.targetDirection(GPIO_0[17]),
+.swivelDistance(swivelDistance),
+.swivelDirection(swivelDirection),
+.reverse(reverse),
+.SW(SW)
+);
+					
+endmodule
